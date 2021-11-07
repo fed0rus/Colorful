39c9544ad91e73ed85e0e934913ae4b88eed2f9f
Ruslan Fedorov <ru.fedorov@innopolis.university>
2021-11-07 17:41:58 +0300

    Update code.ts

--- code.ts
+++ code.ts
@@ -19,7 +19,7 @@ figma.ui.onmessage = msg => {
     for (let i = 0; i < msg.count; i++) {
       const rect = figma.createRectangle();
       rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+      rect.fills = [{type: 'SOLID', color: {r: 0.7, g: 0.1, b: 0}}];
       figma.currentPage.appendChild(rect);
       nodes.push(rect);
     }
